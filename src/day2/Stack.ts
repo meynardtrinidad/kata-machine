af49cbf1fc5fb31098fcae0478d6b29116777b81
Meynard Trinidad <meynard.trinidad@diversifiedinfra.com>
2025-01-01 10:09:01 +0800

    feat: completed stack

--- src/day2/Stack.ts
+++ src/day2/Stack.ts
@@ -1,18 +1,40 @@
+type Node<T> = {
+    value: T
+    next?: Node<T>
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    node?: Node<T>
 
     constructor() {
+        this.length = 0
     }
 
     push(item: T): void {
+        const node: Node<T> = { value: item }
 
-}
+        if (this.node) {
+            node.next = this.node
+        }
+
+        this.node = node
+        this.length++
+    }
+
     pop(): T | undefined {
+        const node = this.node
 
-}
+        if (!node) {
+            return
+        }
+
+        this.node = node.next
+        this.length--
+        return node.value
+    }
+
     peek(): T | undefined {
-
-}
+        return this.node ? this.node.value : undefined
+    }
 }
