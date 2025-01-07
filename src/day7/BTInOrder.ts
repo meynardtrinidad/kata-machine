f08716840fe8ab3fb6c6db2a6d392ce69878f1a9
meynardtrinidad <meynard.trinidad44@gmail.com>
2025-01-07 19:07:52 +0800

    feat: implemented binary tree in order traversal

--- src/day7/BTInOrder.ts
+++ src/day7/BTInOrder.ts
@@ -1,3 +1,21 @@
 export default function in_order_search(head: BinaryNode<number>): number[] {
+    const visitedNodes: number[] = []
+    const stack: BinaryNode<number>[] = []
+    let curr: BinaryNode<number> | null = head
 
+    while (curr != null || stack.length > 0) {
+        while (curr != null) {
+            stack.push(curr)
+            curr = curr.left
+        }
+
+        curr = stack.pop() as BinaryNode<number> | null
+        if (!curr) {
+            continue
+        }
+        visitedNodes.push(curr.value)
+        curr = curr.right
+    }
+
+    return visitedNodes
 }
